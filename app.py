d7ed978edbb248623d12c2a6e29074837c7ce1d1
Ananas0-0 <mpovstemskij@gmail.com.com>
2026-05-31 15:40:47 +0300

    App v2.0 with metrics

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, jsonify
 import time
 
 app = Flask(__name__)
+counter = 0
 
 @app.route('/time')
 def get_time():
+    global counter
+    counter += 1
     return jsonify({"time": int(time.time())})
+
+@app.route('/metrics')
+def get_metrics():
+    return jsonify({"count": counter})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
